766d592932c9ec15433fd0abd45fbe885a75d987
AliTek-03246155728 <alihassanrehmani.6066@gmail.com>
2026-05-09 11:16:18 -0700

    Add files via upload

---
+++ src/data/nftData.js
@@ -0,0 +1,52 @@
+const nftData = [
+  {
+    id: 1,
+    name: "Cyber Ape",
+    artist: "Ali Hassan",
+    price: 2.5,
+    image:
+    "https://i.postimg.cc/yxNpFy1Z/ali-hassan.jpg",
+    description: "Rare futuristic ape NFT artwork."
+  },
+  {
+    id: 2,
+    name: "Neon Samurai",
+    artist: "Ahmed",
+    price: 4.1,
+    image:
+      "https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?q=80&w=800",
+    description: "Cyberpunk samurai in neon city."
+  },
+
+  {
+    id: 3,
+    name: "Digital Queen",
+    artist: "Sara",
+    price: 1.8,
+    image:
+      "https://images.unsplash.com/photo-1642104704074-907c0698cbd9?q=80&w=800",
+    description: "Elegant AI-generated queen portrait."
+  },
+
+  {
+    id: 4,
+    name: "Future Skull",
+    artist: "Usman",
+    price: 3.7,
+    image:
+      "https://images.unsplash.com/photo-1634973357973-f2ed2657db3c?q=80&w=800",
+    description: "Dark futuristic skull NFT."
+  },
+    {
+    id: 5,
+    name: "Future Skull",
+    artist: "Zahid",
+    price: 3.7,
+    image:
+      "https://images.unsplash.com/photo-1635322966219-b75ed372eb01?q=80&w=800",
+    description: "Dark futuristic skull NFT."
+  }
+];
+
+export default nftData;
+
